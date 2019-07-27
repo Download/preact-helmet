513278387d47a517aa6d422efdd048d4cd06e366
Damiano Magrini <30448081+DamianoMagrini@users.noreply.github.com>
2019-07-27 15:17:30 +0200

    Added TS types
    Might still be quite broken

---
+++ index.d.ts
@@ -0,0 +1,45 @@
+/// <reference types="preact" />
+
+import * as preact from 'preact';
+
+declare class PreactHelmet extends preact.Component<
+    PreactHelmet.HelmetProps,
+    any
+> {
+    render(...args: any[]): JSX.Element | null;
+}
+
+declare namespace PreactHelmet {
+    function peek(): PreactHelmet.HelmetData;
+    function rewind(): PreactHelmet.HelmetData;
+
+    interface HelmetProps {
+        base?: any;
+        defaultTitle?: string;
+        htmlAttributes?: any;
+        link?: Array<any>;
+        meta?: Array<any>;
+        script?: Array<any>;
+        style?: Array<any>;
+        title?: string;
+        titleTemplate?: string;
+        onChangeClientState?: (newState: any) => void;
+    }
+
+    interface HelmetData {
+        base: HelmetDatum;
+        htmlAttributes: HelmetDatum;
+        link: HelmetDatum;
+        meta: HelmetDatum;
+        script: HelmetDatum;
+        style: HelmetDatum;
+        title: HelmetDatum;
+    }
+
+    interface HelmetDatum {
+        toString(): string;
+        toComponent(): preact.Component<any, any>;
+    }
+}
+
+export default PreactHelmet;
